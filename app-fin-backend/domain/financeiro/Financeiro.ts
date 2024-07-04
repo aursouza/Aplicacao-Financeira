28a4aa9b995678b04bcbe4fffb8097b01b83cefc
Marco Souza <aursouza28@gmail.com>
2024-07-04 18:17:33 -0300

    fix: alter domain to export props to usecases

--- app-fin-backend/domain/financeiro/Financeiro.ts
+++ app-fin-backend/domain/financeiro/Financeiro.ts
@@ -1,33 +1,62 @@
-import Data from "../shared/Data";
-import Descricao from "../shared/Descricao";
-import Valor from "../shared/Valor";
-import Tipo from "../shared/Tipo";
-import Status from "../shared/Status";
-import { v4 as uuid } from "uuid";
+import Data from '../shared/Data'
+import Descricao from '../shared/Descricao'
+import Valor from '../shared/Valor'
+import Tipo from '../shared/Tipo'
+import Status from '../shared/Status'
 
-export interface FinanceiroProps {
-  id?: string;
-  tipo: string;
-  valor: string | number;
-  status: boolean;
-  data: string | Date;
-  descricao: string;
+export type FinanceiroProps = {
+  id: string
+  tipo: Tipo
+  valor: Valor
+  status: Status
+  data: Data
+  descricao: Descricao
 }
 
 export default class Financeiro {
-  readonly id: string;
-  tipo: Tipo;
-  valor: Valor;
-  status: Status;
-  data: Data;
-  descricao: Descricao;
+  private constructor(private props: FinanceiroProps) {}
 
-  constructor(props: FinanceiroProps) {
-    this.id = props.id ?? uuid();
-    this.tipo = new Tipo(props.tipo);
-    this.valor = new Valor(props.valor);
-    this.status = new Status(props.status);
-    this.data = new Data(props.data);
-    this.descricao = new Descricao(props.descricao);
+  public static create(
+    id: string,
+    tipo: string,
+    valor: number,
+    status: boolean,
+    data: Date,
+    descricao: string
+  ) {
+    return new Financeiro({
+      id: crypto.randomUUID().toString(),
+      tipo: new Tipo(tipo),
+      valor: new Valor(valor),
+      status: new Status(status),
+      data: new Data(data),
+      descricao: new Descricao(descricao),
+    })
+  }
+
+  public static with(props: FinanceiroProps) {
+    return new Financeiro(props)
+  }
+
+  public get id() {
+    return this.props.id
+  }
+
+  public get tipo() {
+    return this.props.tipo
+  }
+
+  public get valor() {
+    return this.props.valor
+  }
+  public get status() {
+    return this.props.status
+  }
+  public get data() {
+    return this.props.data
+  }
+
+  public get descricao() {
+    return this.props.descricao
   }
 }
