deb808f556849522b3718092afd8fdf542492ffc
Marco Souza <aursouza28@gmail.com>
2024-07-04 23:11:55 -0300

    refactor: delete repository moved

--- app-fin-backend/repository/gateway/FinanceiroGateway.ts
+++
@@ -1,8 +0,0 @@
-import Financeiro from 'domain/financeiro/Financeiro'
-
-export interface FinanceiroGateway {
-  save(financeiro: Financeiro): Promise<void>
-  list(): Promise<Financeiro[]>
-  update(financeiro: Financeiro): Promise<Financeiro>
-  delete(id: string): Promise<void>
-}
